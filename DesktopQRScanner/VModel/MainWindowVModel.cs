2719efb4630c7b9dd9a86f4ebb36193a17552e7a
gsc <gsc067218@gmail.com>
2023-04-04 15:50:26 +0800

    添加项目文件。

---
+++ DesktopQRScanner/VModel/MainWindowVModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopQRScanner.VModel
+{
+    internal partial class MainWindowVModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool showPop = false;
+
+        [RelayCommand]
+        private void showPopClick()
+        {
+            ShowPop = true;
+        }
+    }
+}
