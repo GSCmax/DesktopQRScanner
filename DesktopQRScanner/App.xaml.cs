577bd3adf83e0effe3505337a3148a325cd262eb
gsc <gsc067218@gmail.com>
2023-04-13 17:02:48 +0800

    继续开发

--- DesktopQRScanner/App.xaml.cs
+++ DesktopQRScanner/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using DesktopQRScanner.Tools;
 using DesktopQRScanner.VModel;
+using HandyControl.Data;
+using HandyControl.Tools;
 using HinsHo.ScreenShot.CSharp;
 using System.Windows;
 
@@ -24,6 +26,9 @@ namespace DesktopQRScanner
 
             GlobalDataHelper.Init();
 
+            if (GlobalDataHelper.appConfig.UseDarkTheme)
+                UpdateSkin(SkinType.Dark);
+
             if (GlobalDataHelper.appConfig.AutoFullScreenShotOnStart)
                 GlobalDataHelper.bs = Screenshot.CaptureAllScreens();
         }
@@ -33,6 +38,14 @@ namespace DesktopQRScanner
             base.OnExit(e);
 
             GlobalDataHelper.Save();
+        }
+
+        internal void UpdateSkin(SkinType skin)
+        {
+            Resources.MergedDictionaries.RemoveAt(0);
+            Resources.MergedDictionaries.Insert(0, ResourceHelper.GetSkin(skin));
+
+            Current.MainWindow?.OnApplyTemplate();
         }
     }
 }
